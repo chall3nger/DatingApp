3cabe60570b130efc9f45b728f3671c995d0e75b
Sulaiman <sss_omari@yahoo.com>
2021-09-02 12:35:57 +0300

    End of section 14

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -39,7 +39,7 @@ export class MemberDetailComponent implements OnInit {
         small: photo?.url,
         medium: photo?.url,
         big: photo?.url
-      })
+            })
     }
     return imageUrls;
   }
