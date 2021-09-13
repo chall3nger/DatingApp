9549cb54442344dd5b1f7da78cfb47223c2b3156
Sulaiman <sss_omari@yahoo.com>
2021-09-13 10:19:26 +0300

    End of section 18

--- API/InterFaces/ILikesRepository.cs
+++ API/InterFaces/ILikesRepository.cs
@@ -10,6 +10,5 @@ namespace API.InterFaces
         Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
         Task<AppUser> GetUserWithLikes(int userId);
         Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
-
     }
 }
--- API/InterFaces/IMessageRepository.cs
+++ API/InterFaces/IMessageRepository.cs
@@ -19,6 +19,5 @@ namespace API.InterFaces
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
         Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
-        Task<bool> SaveAllAsync();
     }
 }
